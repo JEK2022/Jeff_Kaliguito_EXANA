081de891390dde0d71e0daa8abd889eceb3aaaa0
JEK2022 <107668551+JEK2022@users.noreply.github.com>
2023-08-08 04:24:53 +0200

    Add files via upload

---
+++ fichier.js
@@ -0,0 +1,24 @@
+//enonse 1
+alert("Byenvini !");
+//enonse 2
+console.info("Ou sou challenge");
+//enonse 3
+console.error("Erreur ");
+//enonse 4
+let nom = "Jeff Kaliguito EXANA";
+let age = 26;
+//afichaj avek fonksyon alert
+alert("Mwen se " +nom +" "+ " mwen genyen " + age + " ans ");
+//affichaj nan konsol la
+console.log("Mwen se " +nom +" "+ " mwen genyen " + age + " ans ");
+//enonse 5
+let nombre1=10;
+let nombre2=5;
+//adisyon nomb yo
+console.log("Som nomb yo egal " + nombre1 + nombre2);
+//soustraksyon
+console.log("Rezilta soustraksyon nomb yo egal " + (nombre1-nombre2));
+//miltiplikasyon
+console.log("Rezilta miltiplikasyon nomb yo egal " + nombre1*nombre2);
+//divizyon
+console.log("Rezilta divizyon nomb yo egal " + nombre1/nombre2);
